722b1ef7b75d8f03c4be005244fa6292db51698c
Jong Hwan Na <95013904+JohnSodium@users.noreply.github.com>
2022-05-12 15:34:45 +0900

    Update detect_vehicles.py
    Adjust time frame

--- detect_vehicles.py
+++ detect_vehicles.py
@@ -10,19 +10,21 @@ def sense_front_vehicle(frame_id, file):
 
 
 def sense_vehicle_on_right(lane_id, frame_id, front_y_position, file):
+    # there are no vehicles in front
     if front_y_position == 0:
         vehicle_sensed = file[
             (file["Lane_ID"] == (lane_id + 1)) &
             (file["Frame_ID"] == (file["Frame_ID"][frame_id])) &
-            (file["Local_Y"] >= (file["Local_Y"][frame_id])) &
-            (file["Local_Y"] <= (file["Local_Y"][frame_id] + 100))]
+            (file["Local_Y"] >= (file["Local_Y"][frame_id] - 10)) &
+            (file["Local_Y"] <= (file["Local_Y"][frame_id] + 50))]
+    # if there is a vehicle in front, then the vehicle on the right should not be ahead of it.
     else:
         vehicle_sensed = file[
             (file["Lane_ID"] == (lane_id + 1)) &
             (file["Frame_ID"] == (file["Frame_ID"][frame_id])) &
-            (file["Local_Y"] >= (file["Local_Y"][frame_id])) &
+            (file["Local_Y"] >= (file["Local_Y"][frame_id] - 10)) &
             (file["Local_Y"] <= front_y_position) &
-            (file["Local_Y"] <= (file["Local_Y"][frame_id] + 100))]
+            (file["Local_Y"] <= (file["Local_Y"][frame_id] + 50))]
 
     if not vehicle_sensed.empty:
         return vehicle_sensed
@@ -31,19 +33,21 @@ def sense_vehicle_on_right(lane_id, frame_id, front_y_position, file):
 
 
 def sense_vehicle_on_left(lane_id, frame_id, front_y_position, file):
+    # there are no vehicles in front
     if front_y_position == 0:
         vehicle_sensed = file[
             (file["Lane_ID"] == (lane_id - 1)) &
             (file["Frame_ID"] == (file["Frame_ID"][frame_id])) &
-            (file["Local_Y"] >= (file["Local_Y"][frame_id])) &
-            (file["Local_Y"] <= (file["Local_Y"][frame_id] + 100))]
+            (file["Local_Y"] >= (file["Local_Y"][frame_id] - 10)) &
+            (file["Local_Y"] <= (file["Local_Y"][frame_id] + 50))]
+    # if there is a vehicle in front, then the vehicle on the left should not be ahead of it.
     else:
         vehicle_sensed = file[
             (file["Lane_ID"] == (lane_id - 1)) &
             (file["Frame_ID"] == (file["Frame_ID"][frame_id])) &
-            (file["Local_Y"] >= (file["Local_Y"][frame_id])) &
+            (file["Local_Y"] >= (file["Local_Y"][frame_id] - 10)) &
             (file["Local_Y"] <= front_y_position) &
-            (file["Local_Y"] <= (file["Local_Y"][frame_id] + 100))]
+            (file["Local_Y"] <= (file["Local_Y"][frame_id] + 50))]
 
     if not vehicle_sensed.empty:
         return vehicle_sensed
