733a5ac230e53274e7db12a09a9e729c1738ae7d
Jong Hwan Na <95013904+JohnSodium@users.noreply.github.com>
2022-05-12 17:37:20 +0900

    Update extractcsv.py

--- findswap/extractcsv.py
+++ findswap/extractcsv.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import main
+import findswap
 
 # load the csv file from file location
 file = pd.read_csv('i-80_new.csv')
 
-result = main.check_swaps(file)
+result = findswap.check_swaps(file)
 
 df = pd.DataFrame.from_records(result, columns=["ID", "Frame", "Local_Y", "Previous_Lane", "New_Lane", "Following"])
 df.to_csv("laneswapdata.csv", index=False)
